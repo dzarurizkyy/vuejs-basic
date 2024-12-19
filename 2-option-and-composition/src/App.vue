ce43dde62b76cbdc2ee88bf72063b65b7aee9670
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2024-12-19 23:20:40 +0700

    section 2 : option and composition

---
+++ 2-option-and-composition/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import OptionComponent from './components/OptionComponent.vue'
+import CompositionComponent from './components/CompositionComponent.vue'
+</script>
+
+<template>
+  <h1>Dzaru Rizky Fathan Fortuna</h1>
+  <OptionComponent /> &nbsp;
+  <CompositionComponent />
+</template>
