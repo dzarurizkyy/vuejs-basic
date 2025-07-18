7859eb04659eee1af983eb63c37e2a9088820050
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-07-18 18:42:12 +0700

    finish phase 6: props

---
+++ 006-props/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainButton from './components/MainButton.vue'
+</script>
+
+<template>
+  <div :style="{ display: 'flex', gap: '6px' }">
+    <MainButton :title="`Submit`" :style="{cursor: 'pointer'}" />
+    <MainButton :title="`Test`" :disabled="true" :style="{cursor: 'pointer'}"/>
+    <MainButton :style="{cursor: 'pointer'}"/>
+  </div>
+</template>
---
+++ 006-props/components/MainButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+const {title, disabled} = defineProps(
+    {
+        title: {
+            type: String,
+            default: "Button",
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+      
+    },
+)
+</script>
+
+<template>
+    <button :disabled="disabled">{{ title }}</button>
+</template>
+
+<style scoped></style>
