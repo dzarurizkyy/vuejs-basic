3fe6b13801c1d72bf08ecab9de3496c3e80846ad
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-07-18 23:48:31 +0700

    finish phase 9: conditional rendering

---
+++ 009-conditional-rendering/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { reactive } from "vue"
+const author = reactive({
+  name: "JK Rowling",
+  books: []
+})
+</script>
+
+<template>
+  <div>
+      <div :style="{ 'fontSize': '20px', 'fontWeight': '900' }">{{ author.name }}</div>
+      <div v-if="author.books.length > 1">Have more than one book</div>
+      <div v-if="author.books.length === 1">Have a book</div>
+      <div v-if="author.books.length === 0">Don't have a book</div>
+  </div>
+</template>
