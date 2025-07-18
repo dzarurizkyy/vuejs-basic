bb5c717cb6c93adbe1b134b5eeede9485fb8ced8
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-07-19 00:50:50 +0700

    finish phase 11: class and style bindings

---
+++ 011-class-and-style-bindings/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {ref} from "vue"
+import MainButton from './components/MainButton.vue';
+
+const disable = ref(false)
+const toggledisable = () => disable.value = !disable.value
+</script>
+
+<template>
+  <MainButton title="Click" :disabled="disable" @click="toggledisable" :style="{cursor: 'pointer'}" />
+</template>
---
+++ 011-class-and-style-bindings/components/MainButton.vue
@@ -0,0 +1,30 @@
+<script setup>
+const { title, disabled } = defineProps({
+  title: {
+    type: String,
+    default: "Title"
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <button :disabled="disabled" class="button" :class="{disabled: disabled}">{{ title }}</button>
+</template>
+
+<style scoped>
+  .button {
+    color: white;
+    border: none;
+    font-size: 16px;
+    padding: 10px 15px;
+    background-color: salmon;
+  }
+
+  .disabled {
+    background-color: grey;
+  }
+</style>
