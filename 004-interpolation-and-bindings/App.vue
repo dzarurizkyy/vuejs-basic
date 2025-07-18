a315b0097fd08eaaa616e585723118212e87506e
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-07-18 17:49:34 +0700

    finish phase 4: interpolation and bindings

---
+++ 004-interpolation-and-bindings/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+const idBlog = "blog-1"
+const title = "This is title"
+const description = '<p style="color:gray; font-size: 18px; font-family: arial">This is description</p>'
+
+const isLoading = true
+const button = { id: "1", disabled: isLoading }
+const renderIsLoading = () => {
+    if (isLoading) {
+        return "Loading"
+    } else {
+        return "Submit"
+    }
+}
+</script>
+
+<template>
+    <div :id="idBlog">
+        <div :style="{fontSize: '26px', display: 'flex', flexDirection: 'column', color: 'black', fontFamily: 'arial'}">
+            {{ title }}
+        </div>
+        <div v-html="description" />
+        <button v-bind="button">{{ renderIsLoading() }}</button>
+    </div>
+</template>
