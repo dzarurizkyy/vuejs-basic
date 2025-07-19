188f91b6732bae84de5aee1082271b671b8ca6b4
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-07-19 20:45:30 +0700

    finish phase 16: provide and inject

---
+++ 016-provide-and-inject/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { provide, ref } from 'vue';
+import MainButton from './components/MainButton.vue';
+
+const name = ref("Dzaru")
+const updateName = () => name.value = "Dzaru Rizky Fathan Fortuna"
+provide("name", {name, updateName})
+</script>
+
+<template>
+  <MainButton></MainButton>
+</template>
---
+++ 016-provide-and-inject/components/MainButton.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { inject } from 'vue';
+const nameManage = inject("name")
+</script>
+
+<template>
+  <div>{{ nameManage.name }}</div>
+  <br />
+  <button @click="nameManage.updateName">Update Name</button>
+</template>
