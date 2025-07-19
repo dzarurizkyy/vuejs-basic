c45928248a988869f2e4666b2afa4bc2d69b2e3b
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-07-19 16:22:24 +0700

    finish phase 12: event handling

---
+++ 012-event-handling/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue"
+
+const count = ref(0)
+const increment = (counter) => count.value += counter
+function handleEnter(event) { console.log(event.target.value) }
+</script>
+
+<template>
+  <div :style="{display: 'inline-flex', flexDirection: 'row', gap: '10px'}">
+    <button @mouseover="increment(5)" :style="{fontSize: '25px', padding: '0px 10px', cursor: 'pointer'}">
+      {{ count }}
+    </button>
+    <input type="number" @keydown.enter="handleEnter" :style="{height: '30px'}"/>
+  </div>
+</template>
