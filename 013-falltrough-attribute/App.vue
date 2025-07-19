01d49f210f81b5946b8fec52f9841958f7a0955b
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-07-19 16:23:17 +0700

    finish phase 13: falltrough attribute

---
+++ 013-falltrough-attribute/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+import MainButton from './components/MainButton.vue';
+
+let counter = ref(0)
+const increment = (value) => {
+  counter.value += value
+  console.log(counter.value)
+}
+
+</script>
+
+<template>
+  <MainButton class="my-button" @click="increment(5)" title="World"/>
+</template>
---
+++ 013-falltrough-attribute/components/MainButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useAttrs } from 'vue';
+
+// defineOptions({inheritAttrs: false})
+const { title } = defineProps({ title: { type: String, default: "Title" } })
+const attrs = useAttrs()
+</script>
+
+<template>
+  <span @click="attrs.onClick">Hello &nbsp;</span>
+  <button class="button" :class="attrs.class">{{ title }}</button>
+</template>
+
+<style scoped>
+  .button {
+    color: white;
+    border: none;
+    font-size: 16px;
+    padding: 8px;
+    background-color: grey;
+  }
+</style>
