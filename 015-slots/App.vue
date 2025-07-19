c4fa922fc5de7484a12935544f0d2ed8576368c1
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-07-19 20:08:09 +0700

    finish phase 15: slots

---
+++ 015-slots/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MainButton from './components/MainButton.vue';
+</script>
+
+<template>
+  <MainButton>
+    <template #header>
+      <h1>Hello</h1>
+    </template>
+    <template #default="{title}">
+      <div>{{ title }}</div>
+    </template>
+  </MainButton>
+</template>
---
+++ 015-slots/components/MainButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <button class="button">
+    <div v-if="$slots.header">
+      <slot name="header" />
+    </div>
+    <div>
+      <slot :title="'World'">Submit</slot>
+    </div>
+  </button>
+</template>
+
+<style scoped>
+  .button {
+    color: white;
+    border: none;
+    font-size: 16px;
+    padding: 8px;
+    background-color: grey;
+  }
+</style>
